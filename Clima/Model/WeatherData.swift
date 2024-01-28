4c0c5dde532eebf940f16c0a9a9bec13327eacf1
Ana Ptskialadze <anukiptskialadze@gmai.com>
2024-01-28 14:34:39 +0400

    Project finished.

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Ana Ptskialadze on 12.11.23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+}
